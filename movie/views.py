aaeba8cc1942bb2d2087ac0d82adacdae7c22af3
Diego Gonzalez <142106783+xDiegoAGG@users.noreply.github.com>
2025-02-13 02:52:18 -0500

    cambios en la funcion home.

--- movie/views.py
+++ movie/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Diego Andrés Gonzalez'})
+    return render(request, 'home.html', {'name': 'Diego Gonzalez'})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
