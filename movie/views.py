c4bab8368d0e5327a7ddd28c0c209eb29549ce8d
Diego Gonzalez <142106783+xDiegoAGG@users.noreply.github.com>
2025-02-13 02:46:09 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,12 @@
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {'name': 'Diego Andrés Gonzalez'})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
