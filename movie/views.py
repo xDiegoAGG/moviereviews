25625499fd9c5a06b05d44efb40e19ad6adef5ec
Diego Gonzalez <142106783+xDiegoAGG@users.noreply.github.com>
2025-03-03 01:47:36 -0500

    Últimos detalles

--- movie/views.py
+++ movie/views.py
@@ -3,10 +3,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Diego Gonzalez'})
+    #return render(request, 'home.html', {'name': 'Diego Gonzalez'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    #return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
